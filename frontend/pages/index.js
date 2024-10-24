7f5ee83888b2ac6c81b660f992ff68e95710b204
Yakimelon <aisatu_nomahou@yahoo.co.jp>
2024-10-24 13:27:21 +0900

    フッター位置調整

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -101,7 +101,7 @@ export default function Home() {
                 </button>
             </div>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mb-20">
                 {playersWithReviews.map((player) => (
                     <div
                         key={player.id}
